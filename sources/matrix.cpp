0d93c069b5760ec4d6450a83a109d0abd4b7df07
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2018-03-19 17:18:09 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -1,250 +1,342 @@
-#include <iostream>
-#include <cassert>
+#include <catch.hpp>
+#include <sstream>
 
-template<typename T>
-class matrix_t {
-private:
-    T ** elements_;
-    std::size_t rows_;
-    std::size_t collumns_;
-public:
-    matrix_t();
-    matrix_t( matrix_t const & other );
-    matrix_t & operator =( matrix_t const & other );
-    ~matrix_t();
+#include "matrix.hpp"
+
+TEST_CASE("creating matrix")
+{
+    matrix_t<int> matrix;
+    REQUIRE( matrix.rows() == 0 );
+    REQUIRE( matrix.collumns() == 0 );
+}
+
+TEST_CASE("reading matrix")
+{
+    std::string input{
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    matrix_t<int> matrix;
+    std::istringstream istream{ input };
     
-    std::size_t rows() const;
-    std::size_t collumns() const;
+    REQUIRE( matrix.read( istream ) );
+    REQUIRE( matrix.rows() == 3 );
+    REQUIRE( matrix.collumns() == 3 );
     
-    matrix_t operator +( matrix_t const & other ) const;
-    matrix_t operator -( matrix_t const & other ) const;
-    matrix_t operator *( matrix_t const & other ) const;
+    std::ostringstream ostream;
+    matrix.write( ostream );
     
-    matrix_t & operator -=( matrix_t const & other );
-    matrix_t & operator +=( matrix_t const & other );
-    matrix_t & operator *=( matrix_t const & other );
+    REQUIRE( input == ostream.str() );
+}
+
+
+TEST_CASE("copying matrix")
+{
+    std::string input
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    matrix_t<int> matrix;
+    std::istringstream istream{ input };
+    REQUIRE( matrix.read(istream));
     
-    std::istream & read( std::istream & stream );
-    std::ostream & write( std::ostream  & stream ) const;
-};
-
-template<typename T>
-matrix_t<T>::matrix_t() : elements_{ nullptr }, rows_{ 0 }, collumns_{ 0 }
-{
+    matrix_t<int> copy(matrix);
+    REQUIRE( copy.rows() == 3);
+    REQUIRE( copy.collumns() == 3);
 }
-
-template<typename T>
-matrix_t<T>::matrix_t( matrix_t const & other )
-{
-    elements_ = new T *[other.rows_];
-    for (std::size_t i = 0; i < other.rows_; i++)
-    {
-        elements_[i] = new T [other.collumns_];
-        for (std::size_t j = 0; j < other.collumns_; j++)
-        {
-            elements_[i][j] = other.elements_[i][j];
-        }
-        rows_ = other.rows_;
-        collumns_ = other.collumns_;
-    }
-}
-
-template<typename T>
-matrix_t<T> & matrix_t<T>::operator =( matrix_t const & other )
-{
-    if (this == &other)
-    {
-        return *this;
-    }
-    elements_ = new T *[other.rows_];
-    for (std::size_t i = 0; i < other.rows_; i++)
-    {
-        elements_[i] = new T [other.collumns_];
-        for (std::size_t j = 0; j < other.collumns_; j++)
-        {
-            elements_[i][j] = other.elements_[i][j];
-        }
-        rows_ = other.rows_;
-        collumns_ = other.collumns_;
-    }
-    return *this;
-}
-
-template <typename T>
-matrix_t<T>::~matrix_t()
-{
-    for( std::size_t i = 0; i < rows_; ++i )
-    {
-        delete [] elements_[ i ];
-    }
-    delete [] elements_;
-}
-
-template <typename T>
-std::size_t matrix_t<T>::rows() const
-{
-    return rows_;
-}
-
-template <typename T>
-std::size_t matrix_t<T>::collumns() const
-{
-    return collumns_;
-}
-
-template<typename T>
-matrix_t<T> matrix_t<T>::operator +( matrix_t const & other ) const
-{
-    assert(collumns_ == other.collumns() && rows_ == other.rows());
-    matrix_t result;
-    result.elements_ = new T * [rows_];
-    for (std::size_t i = 0; i < rows_; ++ i)
-    {
-        result.elements_ [i] = new T [collumns_];
-        for (std::size_t j = 0; j < collumns_; ++j)
-        {
-            result.elements_ [i][j] = elements_ [i][j] + other.elements_ [i][j];
-        }
-    }
-    result.rows_ = rows_;
-    result.collumns_ = collumns_;
-    return result;
-}
-
-template<typename T>
-matrix_t<T> matrix_t<T>::operator -( matrix_t const & other ) const
-{
-    assert(collumns_ == other.collumns() && rows_ == other.rows());
-    matrix_t result;
-    result.elements_ = new T * [rows_];
-    for (std::size_t i = 0; i < rows_; ++ i)
-    {
-        result.elements_ [i] = new T [collumns_];
-        for (std::size_t j = 0; j < collumns_; ++j)
-        {
-            result.elements_ [i][j] = elements_ [i][j] - other.elements_ [i][j];
-        }
-    }
-    result.rows_ = rows_;
-    result.collumns_ = collumns_;
-    return result;
-}
-
-template<typename T>
-matrix_t<T> matrix_t<T>::operator *( matrix_t const & other ) const
-{
-    assert(collumns_ == other.rows());
-    matrix_t result;
-    result.elements_ = new T * [rows_];
-    for (std::size_t i = 0; i < rows_; i++)
-    {
-        result.elements_[i] = new T [collumns_];
-        for (std::size_t j = 0; j < collumns_; j++)
-        {
-            result.elements_[i][j] = 0;
-        }
-    }
-    for (std::size_t i = 0; i < rows_; i++)
-    {
-        for (std::size_t j = 0; j < other.collumns_; j++)
-        {
-            for (std::size_t z = 0; z < collumns_; z++)
-            {
-                result.elements_[i][j] += elements_[i][z] * other.elements_[z][j];
-            }
-        }
-    }
-    /* умножение строку первой матрицы на столбец второй, чтобы получить столбец третьей*/
-    result.rows_ = rows_;
-    result.collumns_ = other.collumns_;
-    return result;
-}
-
-template <typename T>
-matrix_t<T> & matrix_t<T>::operator -=( matrix_t const & other )
-{
-    *this = *this - other;
-    return *this;
-}
-
-template <typename T>
-matrix_t<T> & matrix_t<T>::operator +=( matrix_t const & other )
-{
-    *this = *this + other;
-    return *this;
-}
-
-template <typename T>
-matrix_t<T> & matrix_t<T>::operator *=( matrix_t const & other )
-{
-    *this = *this * other;
-    return *this;
-}
-
-template<typename T>
-std::istream & matrix_t<T>::read( std::istream & stream )
-{   std::size_t rows;
-    std::size_t collumns;
-    char symbol;
     
-    bool success = true;
-    if( stream >> rows && stream >> symbol && symbol == ',' && stream >> collumns ) {
-        T ** elements = new T *[ rows ];
-        for( std::size_t i = 0; success && i < rows; ++i ) {
-            elements[ i ] = new T[ collumns ];
-            for( std::size_t j = 0; j < collumns; ++j ) {
-                if( !( stream >> elements[ i ][ j ] ) ) {
-                    success = false;
-                    break;
-                }
-            }
-        }
+TEST_CASE("adding matrix")
+{
+    std::string input
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    std::string input2
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+    std::string input3
+    {
+        "3, 3\n"
+        "2 2 2\n"
+        "3 3 3\n"
+        "4 4 4" };
+    matrix_t<int> matrix1, matrix2;
+    std::istringstream istream1{ input };
+    std::istringstream istream2{ input2 };
+    REQUIRE(matrix1.read(istream1));
+    REQUIRE(matrix2.read(istream2));
+    
+    REQUIRE(matrix1.rows() == 3);
+    REQUIRE(matrix1.collumns() == 3);
         
-        if( success ) {
-            for( std::size_t i = 0; i < rows_; ++i ) {
-                delete [] elements_[ i ];
-            }
-            delete [] elements_;
-            
-            rows_ = rows;
-            collumns_ = collumns;
-            elements_ = elements;
-        }
-        else {
-            for( std::size_t i = 0; i < rows; ++i ) {
-                delete [] elements[ i ];
-            }
-            delete [] elements;
-        }
-    }
-    else {
-        success = false;
-    }
+    REQUIRE(matrix2.rows() == 3);
+    REQUIRE(matrix2.collumns() == 3);
+	
+    REQUIRE_NOTHROW( matrix1.collumns() == matrix2.collumns() && matrix1.rows() == matrix2.rows() );
+	  
+    std::ostringstream stream;
+    (matrix1+matrix2).write(stream);
     
-    if( !success ) {
-        stream.setstate( std::ios_base::failbit );
-    }
-    
-    return stream;
+    REQUIRE( input3 == stream.str());
 }
 
-template <typename T>
-std::ostream & matrix_t<T>::write( std::ostream & stream ) const
+TEST_CASE("adding matrix double")
 {
-    stream << rows_ << ", " << collumns_;
-    for( std::size_t i = 0; i < rows_; ++i ) {
-        stream << '\n';
-        for( std::size_t j = 0; j < collumns_; ++j ) {
-            stream << elements_[ i ][ j ];
-            if( j != rows_ - 1 ) {
-                stream << ' ';
-            }
-        }
-    }
+    std::string input
+    {
+        "3, 3\n"
+        "2.5 2.5 2.5\n"
+        "2.5 2.5 2.5\n"
+        "2.5 2.5 2.5" };
+    std::string input2
+    {
+        "3, 3\n"
+        "2.5 2.5 2.5\n"
+        "2.5 2.5 2.5\n"
+        "2.5 2.5 2.5" };
+    std::string input3
+    {
+        "3, 3\n"
+        "5 5 5\n"
+        "5 5 5\n"
+        "5 5 5" };
+    matrix_t<double> matrix1, matrix2;
+    std::istringstream istream1{ input };
+    std::istringstream istream2{ input2 };
+    REQUIRE(matrix1.read(istream1));
+    REQUIRE(matrix2.read(istream2));
     
-    return stream;
+    REQUIRE(matrix1.rows() == 3);
+    REQUIRE(matrix1.collumns() == 3);
+        
+    REQUIRE(matrix2.rows() == 3);
+    REQUIRE(matrix2.collumns() == 3);
+	
+    REQUIRE_NOTHROW( matrix1.collumns() == matrix2.collumns() && matrix1.rows() == matrix2.rows() );
+	  
+    std::ostringstream stream;
+    (matrix1+matrix2).write(stream);
+    
+    REQUIRE( input3 == stream.str());
 }
 
+TEST_CASE("substracting matrix")
+{
+    std::string input
+    {
+        "3, 3\n"
+        "3 3 3\n"
+        "2 2 2\n"
+        "3 3 3" };
+    std::string input2
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+    std::string input3
+    {
+        "3, 3\n"
+        "2 2 2\n"
+        "1 1 1\n"
+        "2 2 2" };
+    matrix_t<int> matrix1, matrix2;
+    std::istringstream istream1{ input };
+    std::istringstream istream2{ input2 };
+    REQUIRE(matrix1.read(istream1));
+    REQUIRE(matrix2.read(istream2));
+    
+    REQUIRE(matrix1.rows() == 3);
+    REQUIRE(matrix1.collumns() == 3);
+        
+    REQUIRE(matrix2.rows() == 3);
+    REQUIRE(matrix2.collumns() == 3);
+    
+    REQUIRE_NOTHROW( matrix1.collumns() == matrix2.collumns() && matrix1.rows() == matrix2.rows() );
+    
+    std::ostringstream stream;
+    (matrix1-matrix2).write(stream);
+    
+    REQUIRE( input3 == stream.str());
+}
 
+TEST_CASE ("multiplication matrix")
+{
+    std::string input
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    std::string input2
+    {
+        "3, 3\n"
+        "3 3 3\n"
+        "4 4 4\n"
+        "5 5 5" };
+    std::string input3
+    {
+        "3, 3\n"
+        "12 12 12\n"
+        "24 24 24\n"
+        "36 36 36" };
+    matrix_t<int> matrix1, matrix2;
+    std::istringstream istream1{ input };
+    std::istringstream istream2{ input2 };
+    REQUIRE(matrix1.read(istream1));
+    REQUIRE(matrix2.read(istream2));
+    
+    REQUIRE_NOTHROW( matrix1.collumns() == matrix2.rows() );
+        
+    std::ostringstream stream;
+    (matrix1 * matrix2).write( stream );
+    
+    REQUIRE( input3 == stream.str() );
+}
+
+TEST_CASE ("assignment matrix")
+{
+    std::string input
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    std::string input2
+    {
+        "3, 3\n"
+        "3 3 3\n"
+        "4 4 4\n"
+        "5 5 5" };
+    matrix_t<int> matrix1, matrix2;
+    std::istringstream istream1{ input };
+    std::istringstream istream2{ input2 };
+    REQUIRE(matrix1.read(istream1));
+    REQUIRE(matrix2.read(istream2));
+    
+    REQUIRE_NOTHROW( matrix1.collumns() == matrix2.collumns() && matrix1.rows() == matrix2.rows() );
+    
+    matrix1 = matrix2;
+    std::ostringstream ostream;
+    matrix1.write( ostream );
+    
+    REQUIRE( input2 == ostream.str() );
+}   
+
+TEST_CASE ("assignment with multiplication")
+{
+    std::string input
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    std::string input2
+    {
+        "3, 3\n"
+        "3 3 3\n"
+        "4 4 4\n"
+        "5 5 5" };
+    std::string input3
+    {
+        "3, 3\n"
+        "12 12 12\n"
+        "24 24 24\n"
+        "36 36 36" };
+    
+    matrix_t<int> matrix1, matrix2;
+    std::istringstream istream1{ input };
+    std::istringstream istream2{ input2 };
+    REQUIRE(matrix1.read(istream1));
+    REQUIRE(matrix2.read(istream2));
+    
+    REQUIRE_NOTHROW( matrix1.collumns() == matrix2.rows() );
+    
+    matrix1 *= matrix2;
+    std::ostringstream ostream;
+    matrix1.write( ostream );
+    
+    REQUIRE (input3 == ostream.str());
+}
+
+TEST_CASE("assignment with adding")
+{
+    std::string input
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    std::string input2
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+    std::string input3
+    {
+        "3, 3\n"
+        "2 2 2\n"
+        "3 3 3\n"
+        "4 4 4" };
+    
+    matrix_t<int> matrix1, matrix2;
+    std::istringstream istream1{ input };
+    std::istringstream istream2{ input2 };
+    REQUIRE(matrix1.read(istream1));
+    REQUIRE(matrix2.read(istream2));
+    
+    REQUIRE_NOTHROW( matrix1.collumns() == matrix2.collumns() && matrix1.rows() == matrix2.rows() );
+    
+    matrix1 += matrix2;
+    std::ostringstream ostream;
+    matrix1.write( ostream );
+    
+    REQUIRE (input3 == ostream.str());
+}
+
+TEST_CASE("assignment with substraction")
+{
+    std::string input
+    {
+        "3, 3\n"
+        "3 3 3\n"
+        "2 2 2\n"
+        "3 3 3" };
+    std::string input2
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+    std::string input3
+    {
+        "3, 3\n"
+        "2 2 2\n"
+        "1 1 1\n"
+        "2 2 2" };
+    
+    matrix_t<int> matrix1, matrix2;
+    std::istringstream istream1{ input };
+    std::istringstream istream2{ input2 };
+    REQUIRE(matrix1.read(istream1));
+    REQUIRE(matrix2.read(istream2));
+    
+    REQUIRE_NOTHROW( matrix1.collumns() == matrix2.collumns() && matrix1.rows() == matrix2.rows() );
+    
+    matrix1 -= matrix2;
+    std::ostringstream ostream;
+    matrix1.write( ostream );
+    
+    REQUIRE (input3 == ostream.str());
+}
 
 
